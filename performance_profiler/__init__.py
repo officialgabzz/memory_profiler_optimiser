0c2ab6517430ee7190605f45091964a3acab9ad3
Malik GAruba <garubamalik@gmail.com>
2020-04-15 14:30:00 +0100

    Add memory profiling feature
    - Implemented MemoryProfiler class with psutil integration
    - Added memory tracking decorator
    - Track memory allocation, peak usage, and changes
    - System memory information getter
    - Created memory profiling example
    - Updated package version to 0.2.0

--- performance_profiler/__init__.py
+++ performance_profiler/__init__.py
@@ -3,9 +3,10 @@ Performance Profiler & Optimizer
 A comprehensive Python profiling toolkit with auto-optimization capabilities.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Performance Optimizer Team"
 
 from .profiler import PerformanceProfiler
+from .memory_profiler import MemoryProfiler
 
-__all__ = ["PerformanceProfiler"]
+__all__ = ["PerformanceProfiler", "MemoryProfiler"]
