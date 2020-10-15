34124603d81be8ab4125a1e98295e72dcfdd83af
Malik GAruba <garubamalik@gmail.com>
2020-10-15 11:20:00 +0100

    Add auto-optimization suggestion engine
    - Implemented OptimizerEngine with pattern and AST analysis
    - Rule-based optimization detection
    - Profile-based suggestions (time, memory, call frequency)
    - Impact categorization (high, medium, low)
    - Detailed suggestion reporting
    - Function and code analysis capabilities
    - Example demonstrating all optimization features
    - Updated version to 0.4.0

--- performance_profiler/__init__.py
+++ performance_profiler/__init__.py
@@ -3,11 +3,18 @@ Performance Profiler & Optimizer
 A comprehensive Python profiling toolkit with auto-optimization capabilities.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__ = "Performance Optimizer Team"
 
 from .profiler import PerformanceProfiler
 from .memory_profiler import MemoryProfiler
 from .line_profiler import LineByLineProfiler
+from .optimizer import OptimizerEngine, OptimizationSuggestion
 
-__all__ = ["PerformanceProfiler", "MemoryProfiler", "LineByLineProfiler"]
+__all__ = [
+    "PerformanceProfiler", 
+    "MemoryProfiler", 
+    "LineByLineProfiler",
+    "OptimizerEngine",
+    "OptimizationSuggestion"
+]
