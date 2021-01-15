6bfdba2876d6e2ab11bb1ee1b5b674ec3a4560a4
Malik GAruba <garubamalik@gmail.com>
2021-01-15 09:15:00 +0000

    Implement before/after comparison system
    - Created ComparisonEngine for A/B testing
    - ComparisonResult class for storing metrics
    - Time and memory improvement calculations
    - Percentage improvement tracking
    - Visual comparison reporting with emojis
    - JSON export functionality
    - Summary generation across multiple comparisons
    - Comprehensive examples with various optimization scenarios
    - Updated version to 0.5.0

--- performance_profiler/__init__.py
+++ performance_profiler/__init__.py
@@ -3,18 +3,21 @@ Performance Profiler & Optimizer
 A comprehensive Python profiling toolkit with auto-optimization capabilities.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __author__ = "Performance Optimizer Team"
 
 from .profiler import PerformanceProfiler
 from .memory_profiler import MemoryProfiler
 from .line_profiler import LineByLineProfiler
 from .optimizer import OptimizerEngine, OptimizationSuggestion
+from .comparator import ComparisonEngine, ComparisonResult
 
 __all__ = [
     "PerformanceProfiler", 
     "MemoryProfiler", 
     "LineByLineProfiler",
     "OptimizerEngine",
-    "OptimizationSuggestion"
+    "OptimizationSuggestion",
+    "ComparisonEngine",
+    "ComparisonResult"
 ]
